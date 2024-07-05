2772b12e4a0f972d994585b4433fcaf632af52b0
FarruQureshi <148589878+ItsFarmanTech@users.noreply.github.com>
2024-07-05 23:52:40 +0530

    Create Blog-Downloader.js

---
+++ Blog-Downloader.js
@@ -0,0 +1,15 @@
+function checkUniqueId() {
+  var uniqueId = document.getElementById("uniqueID").value;
+  var gdriveLink = "";
+  if (uniqueId === "1ZPGS82IU9XZ6WT7") {
+    gdriveLink = "https://drive.google.com/drive/folders/1Yg1ATn27BaL4U1QZ3irAQMmCPqOYbeMr";
+  } else if (uniqueId === "VPVNH0FYFG7FUADZ") {
+    gdriveLink = "https://drive.google.com/drive/u/1/folders/1IJECHliPuDZ1AoQSSqtHeJyiNVwxOP7c";
+  }
+
+  if (gdriveLink !== "") {
+    var element = window.open(gdriveLink);
+  } else {
+    alert("First, you need to purchase the Download Timer script to generate your unique-ID.");
+  }
+}
